2a613cddd5b774bded7f331a44bc10c04c204e0e
Brooks <Skoorb412785@yahoo.com>
2020-02-21 16:02:06 -0500

    changing props to meet requirements

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -19,3 +19,28 @@ label="Do Something"
 type="primary"
 gradient
 />)
+
+.add('Big Primary', () => <Button
+label="Do Something"
+type="primary"
+big
+/>)
+
+.add('Big Outline Primary', () => <Button
+label="Do Something"
+type="primary"
+outline
+big
+/>)
+
+.add('Big Gradient Primary', () => <Button
+label="Do Something"
+type="primary"
+gradient
+big
+/>)
+
+.add('Danger', () => <Button
+label="Do Something"
+type="danger"
+/>)
